e2dccb5d029dd605ba7dd9dbcfe339cb87509838
Sehaj-kahlon <sehajkahlon437@gmail.com>
2023-07-02 03:23:45 +0530

    animated logo

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wechat/screens/home_screen.dart';
 import '../../main.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -10,7 +11,21 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   @override
+  bool _isAnime = false;
+  //funtion to change the value of _isAnime
+  void initState() {
+    super.initState();
+    //this will let the animation happen after 500 ms
+    Future.delayed(const Duration(milliseconds: 500), () {
+      setState(() {
+        _isAnime = true;
+      });
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    //initializing media query for getting device sreen size
     mq = MediaQuery.of(context).size;
     return Scaffold(
       //appbar
@@ -19,15 +34,16 @@ class _LoginScreenState extends State<LoginScreen> {
         automaticallyImplyLeading: false,
         title: const Text('Welcome to We Chat'),
       ),
-      //place stack any where on the screen
       body: Stack(
-        //posiotiones can be used to position in anywhere on the screen
         children: [
-          Positioned(
+          //app logo
+          AnimatedPositioned(
               top: mq.height * .15,
               width: mq.width * .5,
-              left: mq.width * .25,
+              right: _isAnime ? mq.width * .25 : -mq.width * .5,
+              duration: Duration(seconds: 1),
               child: Image.asset('images/icon.png')),
+          //google login button
           Positioned(
             bottom: mq.height * .15,
             left: mq.width * .05,
@@ -38,7 +54,10 @@ class _LoginScreenState extends State<LoginScreen> {
                     backgroundColor: const Color.fromARGB(255, 223, 255, 187),
                     shape: const StadiumBorder(),
                     elevation: 0.5),
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.pushReplacement(
+                      context, MaterialPageRoute(builder: (_) => HomeScreen()));
+                },
                 icon: Image.asset('images/google.png', height: mq.height * .03),
                 label: RichText(
                   text: const TextSpan(
