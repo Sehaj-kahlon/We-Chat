592975f4f9fe655713527eca998517feaa9deb71
Sehaj-kahlon <sehajkahlon437@gmail.com>
2023-07-04 01:22:49 +0530

    Firebase database created

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -1,5 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class APIs {
+  //for authentication
   static FirebaseAuth auth = FirebaseAuth.instance;
+  //for accessing coud firestore database
+  static FirebaseFirestore firestore = FirebaseFirestore.instance;
 }
